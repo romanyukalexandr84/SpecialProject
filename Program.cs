9790b2c27f0129deb583bf6c8bc8857991fc931f
romanukalexandr <romanukalexandr@gmail.com>
2023-03-20 21:39:54 +0300

    add NewArray method

--- Program.cs
+++ Program.cs
@@ -13,13 +13,45 @@
     {
         Console.Write(mas[i] + " ");
     }
+    Console.WriteLine ();
 }
+
+string[] NewArray (string[] mas)
+{
+int leng = 0;
+for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i].Length <= 3)
+        {
+            leng++;
+        }
+    }
+
+string[] newArr = new string [leng];
+int j = 0;
+
+for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i].Length <= 3)
+        {
+            newArr[j] = mas[i];
+            j++;
+        }
+    }
+return newArr;
+}
+
+
 
 Console.WriteLine ("Введите размерность массива");
 int n = Convert.ToInt32(Console.ReadLine());
 
 string [] array = new string[n];
-
 FillArray (array);
+
+Console.WriteLine ("Первоначальный массив:");
 PrintArray (array);
+Console.WriteLine ();
+
+PrintArray (NewArray (array));
 Console.WriteLine ();
