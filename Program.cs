e2a1fdad31e4204f94cdc47d30abdd9106b59a35
romanukalexandr <romanukalexandr@gmail.com>
2023-03-20 21:11:35 +0300

    add FillArray and PrintArray methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray (string[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine ($"Введите {i}-тый элемент массива");
+        mas[i] = Console.ReadLine();
+    }
+}
+ 
+ void PrintArray (string[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.Write(mas[i] + " ");
+    }
+}
+
+Console.WriteLine ("Введите размерность массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string [] array = new string[n];
+
+FillArray (array);
+PrintArray (array);
+Console.WriteLine ();
